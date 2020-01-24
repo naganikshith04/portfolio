7b67357578b6e5e38b3ecba825ffab79e329b41e
naganikshith04 <41370850+naganikshith04@users.noreply.github.com>
2020-01-24 23:24:55 +0530

    Main Commit
    First Commit

---
+++ mailer/form_setting.php
@@ -0,0 +1,8 @@
+<?php
+	/*Form settings*/
+	$subj = "New message from the site 'vCard & Resume Template'"; //letter subject
+	$to = 'iliana.voynichka@gmail.com'; // Enter Your E-mail
+	$from = 'iliana.voynichka@gmail.com'; // Admin e-mail
+	$fromName = 'Test'; // Your company name
+	$charset = 'UTF-8';
+?>
